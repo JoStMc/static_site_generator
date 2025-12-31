4b865e784597047d8db34277263b30b7e63decc9
JoStMc <joshmcdonnell@hotmail.co.uk>
2025-12-31 22:57:56 +0000

    added parent node and leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,12 +11,32 @@ class HTMLNode:
 
     def props_to_html(self):
         """ Converts the dictionary props to a string """
-        if self.props is None:
-            return
-        output = ""
-        for key in self.props:
-            output += f" {key}=\"{self.props[key]}\""
-        return output
-    
+        if not self.props:
+            return ""
+        return "".join(f' {key}="{value}"' for key, value in self.props.items()) 
+
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, \'{self.props_to_html()}\')"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Parent node must have a tag.")
+        if not self.children:
+            raise ValueError("Node has no children.")
+        return f"<{self.tag}{self.props_to_html()}>{"".join(child.to_html() for child in self.children)}</{self.tag}>"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value.")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -15,8 +15,56 @@ class TestTextNode(unittest.TestCase):
 
         self.assertEqual(example_link.props_to_html(), " href=\"https://www.google.com\" target=\"_blank\"")
         self.assertEqual(example_link2.props_to_html(), " href=\"https://www.google.com\"")
-        self.assertEqual(no_link.props_to_html(), None)
+        self.assertEqual(no_link.props_to_html(), "")
 
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        
+        node1 = LeafNode("a", "google", {"href": "https://www.google.com"})
+        self.assertEqual(node1.to_html(), "<a href=\"https://www.google.com\">google</a>")
+
+        node2 = LeafNode(None, None)
+        with self.assertRaises(ValueError):
+            node2.to_html()
+
+        node3 = LeafNode(None, "raw text")
+        self.assertEqual(node3.to_html(), "raw text")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+        bold_child = LeafNode('b', "bold text")
+        normal_child = LeafNode(None, "normal text")
+        italic_child = LeafNode('i', "italic text")
+        parent_p = ParentNode('p', [bold_child, normal_child, italic_child, normal_child])
+        self.assertEqual(parent_p.to_html(), "<p><b>bold text</b>normal text<i>italic text</i>normal text</p>")
+
+        childless = ParentNode("div", None)
+        tagless = ParentNode("", [child_node])
+        with self.assertRaises(ValueError):
+            childless.to_html()
+            tagless.to_html()
+    
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+        bottom_link = LeafNode('a', "click here", {"href": "https://google.com"})
+        bottom_text = LeafNode(None, "in bold")
+        middle_bold = ParentNode('b', [bottom_text, bottom_link])
+        middle_text = LeafNode(None, "Example of a link")
+        top = ParentNode('p', [middle_text, middle_bold])
+
+        self.assertEqual(top.to_html(), "<p>Example of a link<b>in bold<a href=\"https://google.com\">click here</a></b></p>")
 
 if __name__ == "__main__":
     unittest.main()
